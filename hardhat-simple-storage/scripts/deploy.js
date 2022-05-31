d29945b465317f423bcedb81c79733cfff14dc9c
FerraroSoftware <software.ferraro@gmail.com>
2022-05-31 08:51:21 -0400

    deploy, verifiy, interact with contract via hardhat. lots down in hardhat config

---
+++ hardhat-simple-storage/scripts/deploy.js
@@ -0,0 +1,55 @@
+// imports
+
+// comes with private key / rpc url automatically if not given
+const { verifyMessage } = require("ethers/lib/utils");
+const { ethers, run, network } = require("hardhat");
+
+// async main
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract... ");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+  // console.log(network.config);
+
+  // what happens when we deploy to hardhat, no etherscan
+  // if we on rinkeby and api key exists
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block transactions x6");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value is: ${currentValue}`);
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value is: ${updatedValue}`);
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+}
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
