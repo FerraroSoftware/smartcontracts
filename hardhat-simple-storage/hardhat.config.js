487e69e55ea0ab47aac8b545bcddc6e3a9b933bb
FerraroSoftware <software.ferraro@gmail.com>
2022-05-31 18:49:04 -0400

    add localhost for hardhatnode: ex: yarn hardhat run scripts/deploy.js --network localhost

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -27,6 +27,11 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     rinkeby: { url: RINKEBY_RPC_URL, accounts: [PRIVATE_KEY], chainId: 4 },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      // accounts: Hardhat handles this for us
+    },
   },
   solidity: "0.8.8",
   etherscan: {
