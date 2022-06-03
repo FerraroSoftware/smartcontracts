dfa20970e97b1d755d76031519b38e770cbd55be
FerraroSoftware <software.ferraro@gmail.com>
2022-06-02 20:34:52 -0400

    deploying to rinkeby successfully

--- hardhat-fundme/deploy/01-deploy-fund-me.js
+++ hardhat-fundme/deploy/01-deploy-fund-me.js
@@ -20,6 +20,7 @@ const {
   developmentChains,
 } = require("../helper-hardhat-config");
 const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
@@ -40,12 +41,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   // what about changing chains
   // when going for localhost or hardhat network we want to use a mock
+  const args = [ethUsdPriceFeedAddress];
   const fundMe = await deploy("FundMe", {
     from: deployer,
     args: [ethUsdPriceFeedAddress],
     log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
   });
   log("-------------------------------------------------------------------");
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
 };
 
 module.exports.tags = ["all", "fundme"];
