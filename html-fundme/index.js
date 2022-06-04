cd9ab351a1d2d9c9ae6b41e86697482ac7fe129d
FerraroSoftware <software.ferraro@gmail.com>
2022-06-04 16:32:33 -0400

    connect, balance, withdraw, fund all complete

--- html-fundme/index.js
+++ html-fundme/index.js
@@ -1,5 +1,22 @@
-// connect function for metamask
+// in front end js you cant use require
+// import instead
 
+// react/nextjs we can add via yarn. this is manual
+
+import { abi, contractAddress } from "./constants.js";
+import { ethers } from "./ethers-5.6.esm.min.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
+
+// connect function for metamask
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
@@ -12,5 +29,91 @@ async function connect() {
     console.log(accounts);
   } else {
     connectButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+// async function fund(ethAmount) {
+//   ethAmount = "77";
+//   console.log(`Funding with ${ethAmount}`);
+//   if (typeof window.ethereum !== "undefined") {
+//     // provider / connect to blockchain
+//     // signer / wallet / someone with some gas
+//     // contract that we interact with
+//     // ^ abi and address
+
+//     // shove metamask into provider
+//     const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     // returns wallet connected
+//     const signer = provider.getSigner();
+//     const contract = new ethers.Contract(contractAddress, abi, signer);
+//     const transactionResponse = await contract.fund({
+//       value: ethers.utils.parseEther(ethAmount),
+//     });
+//   }
+// }
+
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value;
+  console.log(`Funding with ${ethAmount}...`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+      // listen for tx to be minded
+      // list for event
+      await listenForTransactionMine(transactionResponse, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+  // call resolve when complete, reject uses timers to reject if to long
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(
+        `Completed with ${transactionReceipt.confirmations} confirmations. `
+      );
+      resolve();
+    });
+  });
+}
+
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    try {
+      const balance = await provider.getBalance(contractAddress);
+      console.log(ethers.utils.formatEther(balance));
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    balanceButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+async function withdraw() {
+  console.log(`Withdrawing...`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransactionMine(transactionResponse, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    withdrawButton.innerHTML = "Please install MetaMask";
   }
 }
