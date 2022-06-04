6d40a3d57ba05d090657947bbf74fa7b4a8d7a7f
FerraroSoftware <software.ferraro@gmail.com>
2022-06-04 11:22:30 -0400

    add integration test, currently not working

---
+++ hardhat-fundme/test/staging/FundMe.staging.test.js
@@ -0,0 +1,32 @@
+// Like intergration test, before you deploy
+const { assert, expect } = require("chai");
+const { network, deployments, ethers, getNamedAccounts } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
+
+// allows only testnets
+developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("FundMe", function () {
+      let fundMe;
+      let deployer;
+      const sendValue = ethers.utils.parseEther("1");
+      beforeEach(async () => {
+        //   accounts = await ethers.getSigners();
+        deployer = (await getNamedAccounts()).deployer;
+        fundMe = await ethers.getContract("FundMe", deployer);
+        //   deployer = accounts[0];
+        //   await deployments.fixture(["fundMe"]);
+        //   fundMe = await ethers.getContract("FundMe", deployer);
+        //   mockV3Aggregator = await ethers.getContract(
+        //     "MockV3Aggregator",
+        //     deployer
+        //   );
+      });
+
+      it("allows people fund and withdraw", async function () {
+        await fundMe.fund({ value: sendValue });
+        await fundMe.withdraw();
+        const endingBalance = await fundMe.provider.getBalance(fundMe.address);
+        assert.equal(endingBalance.toString(), "0");
+      });
+    });
